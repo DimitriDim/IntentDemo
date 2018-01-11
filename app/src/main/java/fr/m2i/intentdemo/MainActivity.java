aa42e6672ae834b6c9e26d943dfa6258823ae433
DimDim <scrat7@gmail.com>
2018-01-11 16:26:38 +0100

    16:20

--- app/src/main/java/fr/m2i/intentdemo/MainActivity.java
+++ app/src/main/java/fr/m2i/intentdemo/MainActivity.java
@@ -3,25 +3,41 @@ package fr.m2i.intentdemo;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeechService;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+
+//implements TextToSpeech.OnInitListener tranforme MainActivity en écouteur
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     EditText txtDomaine;
     TextView txtResult;
     ImageView imgPhoto;
-    int REQUEST_CODE;
+    private final int REQUEST_CODE_LOGIN = 1;
+    private final int REQUEST_CODE_PHOTO = 2;
+    private final int REQUEST_CODE_LIRE = 3;
+    boolean TTS_OK = false;
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         txtDomaine = findViewById(R.id.txtDomain);
+
+        //TTS = Text To Speach
+        // On test si un synthétiseur vocal est présent
+        Intent intLire = new Intent (TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        //on pose la question avec startActivityForResult
+        startActivityForResult(intLire,REQUEST_CODE_LIRE );
     }
 
     public void btnDemo1(View v) {
@@ -39,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void btnDemo2(View v) {
-        REQUEST_CODE = 1;
+
         //création de l'intent
         Intent i = new Intent(this, LoginActivity.class);
 
@@ -48,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Lancer la deuxième activité :
         // REQUEST_CODE est un enteir qui identifie cet appel
-        startActivityForResult(i, REQUEST_CODE);
+        startActivityForResult(i, REQUEST_CODE_LOGIN);
 
     }
 
@@ -57,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         txtResult = findViewById(R.id.txtLogin);
 
-        if (requestCode == 1) { // Tester quelle activité a répondu
+        if (requestCode == REQUEST_CODE_LOGIN) { // Tester quelle activité a répondu
             if (resultCode == RESULT_OK) {
                 String logRecu = data.getStringExtra("Login");
                 txtResult.setText(logRecu);
@@ -65,25 +81,51 @@ public class MainActivity extends AppCompatActivity {
                 txtResult.setText("Non connecté !!");
             }
         }
-        if (requestCode == 2) {
+        if (requestCode == REQUEST_CODE_PHOTO) {
             imgPhoto = findViewById(R.id.imgPhoto);
 
             //"data" de .get("data") est le nom donné à la photo par l'appareil photo (fixé)
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             imgPhoto.setImageBitmap(bitmap);
         }
+
+        if (requestCode == REQUEST_CODE_LIRE) {
+            if(resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS){
+                Toast.makeText(this,"Synthétiseur ok", Toast.LENGTH_LONG).show();
+                TTS_OK = true;
+
+                // le 2e this est la classe MainActivity qu'on vient d'implémenter en écouteur, sinon pas possible
+                tts = new TextToSpeech(this,this);
+
+            }else{
+                Toast.makeText(this,"Synthétiseur KO :(", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public void btnPhoto(View v) {
         // création de l'intent photo
         Intent intPhoto = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        REQUEST_CODE = 2;
 
-        //lancement de l'activité avec une attente de resutat
-        startActivityForResult(intPhoto, REQUEST_CODE);
+        //lancement de l'activité "appareil photo" avec une attente de resutat
+        startActivityForResult(intPhoto, REQUEST_CODE_PHOTO);
 
-        //envoi du resultat vers onActivityResult car activité lancé avec startActivityForResult
-        setResult(RESULT_OK, intPhoto);
     }
 
+    public void btnLire(View v) {
+
+
+        if (TTS_OK = true){
+
+            tts.setLanguage(Locale.ENGLISH);
+            tts.setPitch(10);
+            tts.setSpeechRate(0.5F);
+            tts.speak(txtDomaine.getText().toString(),TextToSpeech.QUEUE_FLUSH,null,null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+
+    }
 }
